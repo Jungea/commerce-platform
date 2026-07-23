049c77c86f82c4ea823f0d65577a24092c050050
Jungea <jungea2241@gmail.com>
2026-07-23 16:29:32 +0900

    feat: Spring Boot 프로젝트 초기 생성

---
+++ src/test/java/org/example/commerceplatform/CommercePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.commerceplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommercePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
